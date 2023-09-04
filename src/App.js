525166a090fb9e0457823335ecd174dd4729162b
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-09-04 08:39:49 -0300

    Add styling with classnames library

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ export default function App() {
         <div>
             <div><Button success primary rounded outline >Click me!</Button></div>
             <div><Button danger outline>Buy now!</Button></div>
-            <div><Button primary secondary outline>See deal!</Button></div>
-            <div><Button secondary rounded>Hide ads!</Button></div>
+            <div><Button primary warning>See deal!</Button></div>
+            <div><Button secondary rounded outline>Hide ads!</Button></div>
+            <div><Button primary rounded>Something</Button></div>
         </div>
     )
 }
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,6 @@
-import propTypes from "prop-types"
+import className from 'classnames';
+
+
 
 export default function Button({
     children,
@@ -10,18 +12,21 @@ export default function Button({
     outline,
     rounded
 }) {
-    return (
-        <button className>{children}</button>
-    )
-}
+    const classes = className('px-3 py-1.5 border', {
+        'border-blue-500 bg-blue-500 text-white': primary,
+        'border-grey-900 bg-gray-900 text-white': secondary,
+        'border-grey-500 bg-green-500 text-white': success,
+        'border-yellow-400 bg-yellow-400 text-white': warning,
+        'border-red-500 bg-red-500 text-white': danger,
+        'border-blue-500 bg-white text-blue-500': outline && primary,
+        'border-gray-500 bg-white text-gray-900': outline && secondary,
+        'border-green-500 bg-white text-green-500': outline && success,
+        'border-yellow-500 bg-yellow-500 text-yellow-400': outline && warning,
+        'border-red-500 bg-white text-red-500': outline && danger,
+        'rounded-full': rounded
+    });
 
-Button.propTypes = {
-    checkVariation: (props) => {
-        const { primary, secondary, success, warning, danger, outline } = props
-        const count = [primary, secondary, success, warning, danger, outline].filter(item => item).length
-        
-        if (count > 1) {
-            return new Error(`Only one variation can be used at a time`)
-        }
-    }
+    return (
+        <button className={classes}>{children}</button>
+    )
 }
