f82cfad5bb8d7c3f18dc65322c16babcd4333c1f
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-10-03 08:58:05 -0300

    Finish Accordion component
    - Add styling to component
    - Use functional-style to update state

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
+import { GoChevronRight, GoChevronDown } from 'react-icons/go';
 
 export default function Accordion({ items }) {
-    const [ expandedIndex, ] = useState(0);
+    const [ expandedIndex, setExpandedIndex ] = useState(0);
+
+    const handleClick = (index) => {
+        // Updates the most recently clicked index
+        setExpandedIndex(currentIndex => currentIndex === index ? -1 : index);
+    }
 
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
+        const icon = <span> {isExpanded ? <GoChevronDown/> : <GoChevronRight/>} </span>;
+
         return (
-            <div key={item.id}>
-                <div>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+            <div key={item.id} onClick={() => handleClick(index)}> 
+                <div className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer' >
+                    {item.label}
+                    {icon}
+                </div>
+                {isExpanded && <div className='border-b p-5'>{item.content}</div>}
             </div>  
         )
     });
