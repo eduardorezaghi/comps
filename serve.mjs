296f4396556324f3a505eea980ccb10e133da9f4
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-10-03 08:36:18 -0300

    Add tailwind-css

--- serve.mjs
+++ serve.mjs
@@ -2,10 +2,11 @@
 import esbuild from "esbuild";
 
 let context = await esbuild.context({
-    entryPoints: ["./src/index.js"],
-    outfile: "./public/assets/app.js",
+    entryPoints: ["./src/index.js", "./src/index.css"],
+    outdir: "public",
     bundle: true,
     sourcemap: 'inline',
+    resolveExtensions: [".tsx", ".ts", ".jsx", ".js", ".css", ".json"],
     loader: {
         ".js": "jsx",
     },
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 
 const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el)
